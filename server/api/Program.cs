a5d85985e0fbc58b371dc976ebce377e12860975
JC Castillo <castillochristopherjames@gmail.com>
2024-09-01 17:53:13 +0800

    Initialized auth

--- server/api/Program.cs
+++ server/api/Program.cs
@@ -1,5 +1,8 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using System.Text;
 using repo;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,13 +13,43 @@ builder.Configuration
        .AddEnvironmentVariables()
        .Build();
 
+builder.Services.AddControllers();
+
 // inject DB Context
 builder.Services.AddDbContext<PostgresContext>(o => {
        o.UseNpgsql(builder.Configuration["DB_KEY"]);
 });
 
+// configure auth
+builder.Services.AddAuthorization();
+builder.Services.AddAuthentication(o => {
+       o.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+       o.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+       o.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(o => {
+       o.IncludeErrorDetails = true;
+       o.SaveToken = true;
+       o.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+       {
+              ValidateIssuerSigningKey = true,
+              IssuerSigningKey = new SymmetricSecurityKey(
+              Encoding.UTF8.GetBytes(builder.Configuration["Supabase:JWT"])
+              ),
+              ValidateIssuer = false,
+              ValidateAudience = true,
+              ValidAudience = "authenticated",
+              ValidIssuer = $"https://{builder.Configuration["Supabase:ProjectId"]}.supabase.co/auth/v1"
+       };
+       o.Events = new JwtBearerEvents();
+});
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello, world!");
+app.UseStaticFiles();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapDefaultControllerRoute();
 
 app.Run();
