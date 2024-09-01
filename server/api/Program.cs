5fe5558796fb03058f8da9e06223c5b4d196c188
JC Castillo <castillochristopherjames@gmail.com>
2024-09-01 17:45:06 +0800

    injected DB in Program.cs

--- server/api/Program.cs
+++ server/api/Program.cs
@@ -1,10 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using repo;
+
 var builder = WebApplication.CreateBuilder(args);
 
+// inject Environment Variables from appsetings.Environment.json
 builder.Configuration
        .SetBasePath(Directory.GetCurrentDirectory())
        .AddJsonFile("appsettings.Environment.json", true)
        .AddEnvironmentVariables()
-       .Build();     
+       .Build();
+
+// inject DB Context
+builder.Services.AddDbContext<PostgresContext>(o => {
+       o.UseNpgsql(builder.Configuration["DB_KEY"]);
+});
 
 var app = builder.Build();
 
