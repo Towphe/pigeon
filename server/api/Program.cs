b23d7db9894bff5de297f339dac06f6e62f0139d
JC Castillo <castillochristopherjames@gmail.com>
2024-09-01 16:22:35 +0800

    setup Environment variable

--- server/api/Program.cs
+++ server/api/Program.cs
@@ -1,6 +1,13 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration
+       .SetBasePath(Directory.GetCurrentDirectory())
+       .AddJsonFile("appsettings.Environment.json", true)
+       .AddEnvironmentVariables()
+       .Build();     
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => "Hello, world!");
 
 app.Run();
